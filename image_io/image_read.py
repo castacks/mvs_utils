84e619c59a3c6a80407b18460049aa8195ad5cb3
Yaoyu Hu <yaoyuh@andrew.cmu.edu>
2022-10-31 01:46:12 -0400

    Update image io.

--- image_io/image_read.py
+++ image_io/image_read.py
@@ -5,8 +5,16 @@ from .float_type import c4_uint8_as_float
 
 def read_image(fn, dtype=np.uint8):
     img = cv2.imread(fn, cv2.IMREAD_UNCHANGED)
-    assert img is not None, f'Failed to read {fn}'
+    assert img is not None, f'Failed to read {fn}. '
     return img.astype(dtype)
+
+def read_image_gray(fn):
+    img = cv2.imread(fn, cv2.IMREAD_GRAYSCALE)
+    assert img is not None, f'Failed to read {fn}. '
+    return img
+
+def read_mask(fn, valid_value=255):
+    return read_image_gray(fn) == valid_value
 
 def read_compressed_float(fn):
     img = read_image(fn, np.uint8)
