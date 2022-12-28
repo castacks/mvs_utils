fd1a5899f7408dc232b52a804a2222daa3bf943b
yaoyuh <yaoyuh@andrew.cmu.edu>
2022-12-27 22:55:29 -0500

    Update debug.py.

--- debug.py
+++ debug.py
@@ -11,6 +11,15 @@ def caller_line():
     frame_info = inspect.stack()[2]
     return f'{frame_info.filename}:{frame_info.lineno}'
 
+def show_msg(msg):
+    str_caller_line = caller_line()
+    print(f'\n>>> DEBUG >>> {str_caller_line}: \n{msg}')
+
+def show_obj(**kwargs):
+    str_caller_line = caller_line()
+    for key, value in kwargs.items():
+        print(f'\n>>> DEBUG >>> {str_caller_line}: \n{key}: {value}')
+
 def show_sum(**kwargs):
     str_caller_line = caller_line()
     
