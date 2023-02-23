15f8ad12ad6555c501077263ad6d2dacd2a4b682
yaoyuh <yaoyuh@andrew.cmu.edu>
2023-02-22 22:32:41 -0500

    Fix the bug in depth_2_distance.py. Minor modification of image_io/image_read.py.

--- depth_2_distance.py
+++ depth_2_distance.py
@@ -1,32 +1,55 @@
-import torch
-import cv2
-
 import numpy as np
+import torch
 
 class Depth2Distance(object):
 
     def __init__(self, camera_model):
         self.camera_model = camera_model
+        self._device = self.camera_model.device
+        
+        # Pixel coordiantes in [2, H, W]. Contiguous by default.
         self.pixel_coords = self.camera_model.pixel_coordinates()
-        self.pixel_coords = self.pixel_coords.reshape((*self.camera_model.shape,2)).cuda()
+
+        self.xyz = torch.zeros( 
+                ( 3, *self.pixel_coords.shape[:2] ), device=self._device, dtype=torch.float32 )
+        
+    @property
+    def device(self):
+        return self._device
+    
+    @device.setter
+    def device(self, d):
+        self.camera_model.device = d
+        self.pixel_coords = self.pixel_coords.to(d)
+        self._device = d
 
     def __call__(self, z):
+        '''
+        z: NumPy array [H, W, 1] or PyTorch tensor [1, H, W]. 
+        '''
 
-        z = torch.from_numpy(z).cuda().squeeze(2)
-        xyz = torch.zeros((*self.pixel_coords.shape[:2],3))
+        if isinstance(z, np.ndarray):
+            z = torch.from_numpy(z).permute((2, 0, 1))
+        
+        if not isinstance(z, torch.Tensor):
+            raise TypeError(f'z must be a NumPy array or PyTorch tensor, but got {type(z)}. ')
+        
+        z = z.to(device=self.device).squeeze(0)
+        
+        xyz = self.xyz.detach().clone()
 
-        u = self.pixel_coords[:,:,0]
-        v = self.pixel_coords[:,:,1]
+        u = self.pixel_coords[0, :, :]
+        v = self.pixel_coords[1, :, :]
 
         fx = self.camera_model.fx
         fy = self.camera_model.fy
         cx = self.camera_model.cx
         cy = self.camera_model.cy
 
-        xyz[:,:,0] = (z / fx) * (u - cx)
-        xyz[:,:,1] = (z / fy) * (v - cy)
-        xyz[:,:,2] = z
+        xyz[0, :, :] = (z / fx) * (u - cx)
+        xyz[1, :, :] = (z / fy) * (v - cy)
+        xyz[2, :, :] = z
 
-        distance = torch.norm(xyz,dim=2)
+        distance = torch.norm(xyz, dim=0)
 
         return distance.cpu().numpy()
